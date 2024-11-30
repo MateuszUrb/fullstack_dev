cf5c3ea1c66a4051050396d237bafa39855f142a
Mateusz Urban <98urban.mateusz@gmail.com>
2024-11-30 18:13:10 +0100

    add: courseinfo, delete unused css

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,28 +1,18 @@
 function App() {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 }
@@ -30,24 +20,28 @@ function App() {
 function Header({ course }) {
   return <h1>{course}</h1>;
 }
-function Content(props) {
+function Content({ parts }) {
   return (
     <div>
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
+      {parts.map(({ name, exercises }) => {
+        return <Part key={name} name={name} exercises={exercises} />;
+      })}
     </div>
   );
 }
-function Total({ exercises1, exercises2, exercises3 }) {
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+function Total({ parts }) {
+  return (
+    <p>
+      Number of exercises {parts.reduce((sum, item) => item.exercises + sum, 0)}
+    </p>
+  );
 }
 
-function Part(props) {
+function Part({ name, exercises }) {
   return (
     <div>
       <p>
-        {props.part} {props.exercise}
+        {name} {exercises}
       </p>
     </div>
   );
